da0cc456a31fd76e8b8327b9b356442cefd2e4b2
Agonpas <agonpas@gmail.com>
2023-11-21 18:55:23 +0100

    Ejercicio 1 acabado

---
+++ Nivel 1/Ejercicio 1/ejercicio1.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_GET['nombre'];
+$apellido = $_GET['apellido'];
+$usuario = $_GET['usuario'];
+echo "El nombre es " . $nombre . "<br/>";
+echo "El apellido es " . $apellido . "<br/>";
+echo "El nombre de usuario es " . $usuario . "<br/>";
+?>
