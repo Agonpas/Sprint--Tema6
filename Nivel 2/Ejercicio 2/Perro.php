69e1c63391a3d23a1418d9e21eb7415eb5e50062
Agonpas <agonpas@gmail.com>
2023-11-25 21:12:27 +0100

    Más comentarios añadidos

--- Nivel 2/Ejercicio 2/Perro.php
+++ Nivel 2/Ejercicio 2/Perro.php
@@ -3,16 +3,16 @@ class Perro{
     /* creamos una array para almacenar los atributos esto nos permite crearlos si predefinirlos*/
     private $atributos = array();
     /* implementamos el __set que nos dejará no solo ajustar el atributo, sino que además lo podremos crear en el caso de no existir*/
-    public function __set($nombre, $valor){
+    public function __set($nombre, $valor){ //declaramos como se llamará el atributo y el valor que tendrá
         echo "Creamos el atributa " . $nombre . " y le damos el valor de " . $valor . PHP_EOL;
-        $this->atributos[$nombre] = $valor;
+        $this->atributos[$nombre] = $valor; //Creamos y setteamos
     }
      /* implementamos el __get que nos mostrará los atributos, en el caso de no existir dicho atributo nos indicará que no existe*/
-    public function __get($nombre){
-        if (array_key_exists($nombre, $this->atributos)) {
-            return $this->atributos[$nombre];
+    public function __get($nombre){ //recibe el nombre del atributo
+        if (array_key_exists($nombre, $this->atributos)) { //busca el atributo en el array y si lo encuntra
+            return $this->atributos[$nombre]; // devolvemos el valor
         } else {
-            echo "El atributo no ha sido creado" . PHP_EOL;
+            echo "El atributo no ha sido creado" . PHP_EOL; //si no lo encuentra lo idicamos
         }
     }
     /*aplicando esta función nos mostrará el estado actual del array con los atributos creados y sus valores*/
